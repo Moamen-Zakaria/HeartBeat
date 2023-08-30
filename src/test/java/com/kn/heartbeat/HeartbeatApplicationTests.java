daeebef618a90efc702ac83ac8dad35e0b078422
mzakaria <moamen.zakaria@gmail.com>
2023-08-30 16:54:02 +0300

    heart beat skeleton

---
+++ src/test/java/com/kn/heartbeat/HeartbeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kn.heartbeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartbeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
